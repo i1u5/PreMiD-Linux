ab8e9790cbd3b41634850ad76f8dcb73dbd39b56
doomlerd <doomlordcs@gmail.com>
2020-02-04 22:57:03 +0100

    ⚡️ [REL] Another test

--- src/managers/trayManager.ts
+++ src/managers/trayManager.ts
@@ -6,67 +6,70 @@ import { connected } from "./socketManager";
 
 let trayIcon = join(__dirname, "../assets/tray/Icon@2x.png");
 export class TrayManager {
-	tray: Tray;
+  tray: Tray;
 
-	constructor() {
-		this.tray = new Tray(trayIcon);
-		this.tray.setToolTip(app.name);
+  constructor() {
+    this.tray = new Tray(trayIcon);
+    this.tray.setToolTip(app.name);
 
-		this.tray.on("right-click", () => this.update());
-	}
+    this.tray.on("right-click", () => this.update());
+  }
 
-	update() {
-		this.tray.setContextMenu(
-			Menu.buildFromTemplate([
-				{
-					icon: join(__dirname, "../assets/tray/Icon.png"),
-					label: `${app.name} v${app.getVersion()}`,
-					enabled: false
-				},
-				{
-					id: "connectInfo",
-					label: `Extension - ${connected ? "Connected" : "Not connected"}`,
-					enabled: false
-				},
-				{
-					type: "separator"
-				},
-				{
-					label: "Presence Store",
-					click: () => shell.openExternal("https://premid.app/store")
-				},
-				{
-					type: "separator"
-				},
-				{
-					label: "Check for Updates",
-					click: () => checkForUpdate(),
-					visible: !updateProcess || (updateProcess && updateProcess == "standby")
+  update() {
+    this.tray.setContextMenu(
+      Menu.buildFromTemplate([
+        {
+          icon: join(__dirname, "../assets/tray/Icon.png"),
+          label: `${app.name} v${app.getVersion()}`,
+          enabled: false
         },
-				{
+        {
+          id: "connectInfo",
+          label: `Extension - ${connected ? "Connected" : "Not connected"}`,
+          enabled: false
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "Presence Store",
+          click: () => shell.openExternal("https://premid.app/store")
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "Check for Updates",
+          click: () => checkForUpdate(),
+          visible:
+            app.isPackaged &&
+            app.name.includes("Portable") &&
+            (!updateProcess || (updateProcess && updateProcess == "standby"))
+        },
+        {
           label: `Update ${app.name}`,
           click: () => update(),
-					visible: (updateProcess && updateProcess == "available")
+          visible: updateProcess && updateProcess == "available"
         },
-				{
+        {
           label: `Updating...`,
           enabled: false,
-					visible: (updateProcess && updateProcess == "installing")
-				},
-				{
-					label: "Acknowledgments",
-					click: () => shell.openExternal("https://premid.app/contributors")
-				},
-				{
-					type: "separator"
-				},
-				{
-					label: `Quit ${app.name}`,
-					role: "quit"
-				}
-			])
-		);
-	}
+          visible: updateProcess && updateProcess == "installing"
+        },
+        {
+          label: "Acknowledgments",
+          click: () => shell.openExternal("https://premid.app/contributors")
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: `Quit ${app.name}`,
+          role: "quit"
+        }
+      ])
+    );
+  }
 }
 
 app.once("quit", () => trayManager.tray.destroy());
